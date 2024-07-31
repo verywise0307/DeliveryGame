a7a39325e534578896c9b005f7a02cefddb9abad
ajdwn <ajdwn07@gmail.com>
2024-07-31 22:23:29 +0900

    게시판을 만들기

---
+++ DeliveryGame/Assets/Gimmick/Scripts/notice.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notice : MonoBehaviour
+{
+    //근처에 가서 f누르면 상호작용
+    //상호작용 -> 아무 화면 띄우기
+    //x버튼 또는 esc 누르면 닫힘
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
